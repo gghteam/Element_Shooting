42c79f497e1a357c5934256cce947bb41870944c
DESKTOP-OHD6DA9\USER <khjgogle@gmail.com>
2022-06-13 10:48:40 +0900

    Bomb Trap

---
+++ Assets/TrapBomb.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapBomb : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer magic;
+    [SerializeField]
+    private ParticleSystem bombParticle;
+
+    [SerializeField]
+    float duration = 2; 
+    [SerializeField]
+    float smoothness = 0.02f;
+
+    bool isBomb = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            if (!isBomb)
+            {
+                StartCoroutine(LerpColor(Color.red, true));
+                isBomb = true;
+            }
+        }
+    }
+
+    IEnumerator LerpColor(Color color, bool isCheck)
+    {
+        float progress = 0;
+        float increment = smoothness / duration;
+        Color OriColor = magic.color;
+        while (progress < 1)
+        {
+            magic.color = Color.Lerp(OriColor, color, progress);
+            progress += increment;
+            yield return new WaitForSeconds(smoothness);
+        }
+        isBomb = isCheck;
+        if (isBomb)
+        {
+            bombParticle.Play();
+            StartCoroutine(LerpColor(Color.white, false));
+        }
+    }
+}
