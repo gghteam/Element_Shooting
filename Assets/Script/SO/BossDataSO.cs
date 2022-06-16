32613e39e22050b333d58ea4f4c512c34e87bdbe
ehddbs <ehddbs295@gmail.com>
2022-06-17 01:57:42 +0900

    BoosRoom Add

---
+++ Assets/Script/SO/BossDataSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="SO/Boss/BossData")]
+public class BossDataSO : ScriptableObject
+{
+    public int health;
+    public float moveSpeed;
+
+}
